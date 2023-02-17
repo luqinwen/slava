79118a3604637004dd719f174daf8127cb0a6a21
qinwen <embiid99@foxmail.com>
2023-02-17 17:46:39 +0800

    0-032&&0-012:init CI and support sortedset in database
    Add .circleci/config.yml

--- pkg/database/tx_utils.go
+++ pkg/database/tx_utils.go
@@ -1,6 +1,9 @@
 package database
 
-import "slava/internal/utils"
+import (
+	"slava/internal/utils"
+	"strconv"
+)
 
 func ReadFirstKey(args [][]byte) ([]string, []string) {
 	key := string(args[0])
@@ -40,4 +43,32 @@ func RollbackGivenKeys(db *DB, keys ...string) []Cmdline {
 		}
 	}
 	return undoCmdLine
+}
+
+func RollbackZSetFields(db *DB, key string, fields ...string) []Cmdline {
+	var undoCmdLines [][][]byte
+	zset, errReply := db.getAsSortedSet(key)
+	if errReply != nil {
+		return nil
+	}
+	if zset == nil {
+		undoCmdLines = append(undoCmdLines,
+			utils.ToCmdLine("DEL", key),
+		)
+		return undoCmdLines
+	}
+	for _, field := range fields {
+		elem, ok := zset.Get(field)
+		if !ok {
+			undoCmdLines = append(undoCmdLines,
+				utils.ToCmdLine("ZREM", key, field),
+			)
+		} else {
+			score := strconv.FormatFloat(elem.Score, 'f', -1, 64)
+			undoCmdLines = append(undoCmdLines,
+				utils.ToCmdLine("ZADD", key, score, field),
+			)
+		}
+	}
+	return undoCmdLines
 }
